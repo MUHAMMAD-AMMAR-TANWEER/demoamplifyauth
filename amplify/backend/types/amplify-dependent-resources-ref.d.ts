8c1dff935cd8b49da94bfc4f1d2ac4352ba2f4fc
MUHAMMAD-AMMAR-TANWEER <ammartanweer7370@gmail.com>
2023-03-13 11:12:52 -0700

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifynextjseb591acf": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
